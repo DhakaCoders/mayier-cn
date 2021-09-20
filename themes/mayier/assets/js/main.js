b61fb00cde8ad7f1338338ed74e743d025511448
Shariful700453 <56506215+Shariful700453@users.noreply.github.com>
2021-09-20 11:48:26 +0600

    ,,,

--- themes/mayier/assets/js/main.js
+++ themes/mayier/assets/js/main.js
@@ -225,6 +225,21 @@ google.maps.event.addDomListener(window, 'load', initialize);
   }, false);
 })();
 
+
+/*start of Kashob*/
+
+
+
+
+/*starto of Noyon*/
+
+
+
+/*start of Shariful*/
+
+
+
+
     new WOW().init();
 
 })(jQuery);
