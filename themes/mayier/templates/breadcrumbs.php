6eec490125d58721e04add6fdcc5b8e3b4c2d053
Azizul Haque <azizul.softsw@gmail.com>
2021-09-22 12:58:27 +0600

    404

---
+++ themes/mayier/templates/breadcrumbs.php
@@ -0,0 +1,14 @@
+<section class="breadcrumb-sec">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="breadcrumb-sec-inr">
+          <ul class="reset-list clearfix">
+            <li class="home"><a href="#"><span>home</span></a></li>
+            <li><span>Error</span></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
